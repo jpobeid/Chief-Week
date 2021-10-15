f3528ff942eef43cb0cae05c49d42f9673c77b78
JP Obeid <jpobeid91@gmail.com>
2021-10-15 16:03:53 -0700

    Returning docx

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from main import run_main
 from asgiref.wsgi import WsgiToAsgi
 from hypercorn.config import Config
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 async def get_email():
     await run_main()
-    return 'All done!!!'
+    return send_from_directory('', 'weekly_email.docx', as_attachment=True)
 
 if __name__ == '__main__':
     asgi_app = WsgiToAsgi(app)
