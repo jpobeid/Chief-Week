66a3556a284d9014ad638ad63befab7aa56b725e
JP Obeid <jpobeid91@gmail.com>
2021-10-17 13:35:28 -0700

    Switched to Quart

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from flask import Flask, send_from_directory
 from main import run_main
-from asgiref.wsgi import WsgiToAsgi
-from hypercorn.config import Config
-from hypercorn.asyncio import serve
-import asyncio
 
-app = Flask(__name__)
+from quart import Quart, send_from_directory
+
+app = Quart(__name__)
 
 @app.route('/', methods=['GET'])
 async def get_email():
     await run_main()
-    return send_from_directory('', 'weekly_email.docx', as_attachment=True)
+    return await send_from_directory('', 'weekly_email.docx', as_attachment=True)
 
 if __name__ == '__main__':
-    asgi_app = WsgiToAsgi(app)
-    asyncio.run(serve(asgi_app, Config()))
+    app.run(debug=True)
