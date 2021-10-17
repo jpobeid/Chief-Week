27c6bc9290833e3c040cfc99abf857c89f1b4266
JP Obeid <jpobeid91@gmail.com>
2021-10-17 16:12:19 -0700

    Added route suffix

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from main import run_main
+from lib import global_vars as globals
 
 from quart import Quart, send_from_directory
 
 app = Quart(__name__)
 
-@app.route('/', methods=['GET'])
+route_suffix = globals.FILENAME_DOCX.split('.')[0]
+@app.route(f'/{route_suffix}', methods=['GET'])
 async def get_email():
     await run_main()
-    return await send_from_directory('', 'weekly_email.docx', as_attachment=True)
+    return await send_from_directory('', globals.FILENAME_DOCX, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=False)
